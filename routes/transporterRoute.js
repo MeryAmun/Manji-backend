07e13c22bd753ead9aa8d14560d616d7cb68f196
MeryAmun <chebesiawah@gmail.com>
2021-06-18 15:34:56 +0100

    some correction

--- routes/transporterRoute.js
+++ routes/transporterRoute.js
@@ -1,13 +1,43 @@
 const express = require('express');
-const data = require('../data');
+const transporter = require('../models/transportermodel');
+const getToken = require('../utils')
 
-const transRouter = express.Router();
+const router = express.Router();
 
-
-transRouter.get('/transporters', (req, res) => {
-    res.send(data.transporters)
+//get transporters from server or db
+router.get("/", async (req, res) => {
+    const transporters = await transporter.find({});
+    res.send(transporters)
 });
 
 
+router.get("/", async (req, res) => {
+    const transporters = await transporter.find({});
+    res.send(transporters)
+});
 
-module.exports = transRouter;
+
+//to add transporters or create create
+router.post("/", async (req, res) =>{
+    const transporter = new Transporter({
+name: req.body.name,
+category: req.body.category,
+image: req.body.image,
+city: req.body.city,
+telephone: req.body.telephone,
+location: req.body.location,
+rating: req.body.rating,
+    });
+});
+const newTransporter = await transporter.save();
+    if (newTransporter) {
+        return res.status(201).send({message: "New transporter created", data:newTransporter });
+    }
+    return res.status(500).send({message: "Error in creating transporter"})
+
+    // all of the script.... 
+
+
+
+
+module.exports = router;
